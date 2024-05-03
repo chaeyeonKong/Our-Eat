20f8789aa7a996a9f9cad2b3f2c69a9f4eb0d59a
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-05-04 06:32:04 +0900

    [feat] 로그인 post 요청

---
+++ lib/service/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const baseUrl = "http://3.36.123.200:8080";
+
+// 로그인
+Future<Map<String, dynamic>> login(
+  String userName,
+  String password,
+) async {
+  final url = Uri.parse('$baseUrl/api/v1/user/login');
+  final data = jsonEncode({
+    'userName': userName,
+    'password': password,
+  });
+  try {
+    http.Response response = await http.post(url,
+        headers: {"Content-Type": "application/json"}, body: data);
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    return jsonData;
+  } catch (error) {
+    print('error: $error');
+    throw Error();
+  }
+}
