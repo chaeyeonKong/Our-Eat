a37f08e8085221c324c5d73ad472885b2c64272c
blackcat417 <ssoo174@ewha.ac.kr>
2024-05-04 00:43:03 +0900

    loginPage

--- lib/screen/signIn.dart
+++ lib/screen/signIn.dart
@@ -12,46 +12,85 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
+      body: Stack(
+        fit: StackFit.expand,
+      children: [
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('./assets/background1.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        ),
+      Container(
+        color: Colors.black.withOpacity(0.7),
+        ),
+      Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Align(
               alignment: Alignment.center,
               child: Text(
-                'Login',
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                '로그인',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 50.0),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.white.withOpacity(0.4), // 흰색 배경에 불투명하게 설정
+              ),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: '아이디',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
               ),
             ),
             SizedBox(height: 10.0),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: '아이디',
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.white.withOpacity(0.4), // 흰색 배경에 불투명하게 설정
               ),
-            ),
-            SizedBox(height: 15.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: '비밀번호',
+              child: TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: '비밀번호',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
               ),
-              obscureText: true,
             ),
             SizedBox(height: 15.0),
             SizedBox(
               width:200.0,
               child: ElevatedButton(
                 onPressed: (){
-
+                  //눌렀을 때 로직 구현
                 },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white.withOpacity(0.8), // 버튼의 배경색을 불투명 흰색으로 설정
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0), // 버튼의 테두리를 동그랗게 설정
+                  ),
+                ),
                 child: Text('로그인'),
-              )
-            )
+              ),
+            ),
           ],
         ),
       ),
+    ],
+    ),
     );
   }
 }
