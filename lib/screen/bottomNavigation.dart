13ad640c36ec01a7f8d33de5e7e78a1b8bfb9004
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-05-04 00:00:18 +0900

    [feat] 하단바 생성 및 클릭 이벤트 생성

---
+++ lib/screen/bottomNavigation.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MaterialApp을 추가하여 Directionality를 제공합니다.
+      home: const GoogleBottomBar(),
+    );
+  }
+}
+
+class GoogleBottomBar extends StatefulWidget {
+  const GoogleBottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<GoogleBottomBar> createState() => _GoogleBottomBarState();
+}
+
+class _GoogleBottomBarState extends State<GoogleBottomBar> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Google Bottom Bar')),
+      body: Center(
+        child: _navBarItems[_selectedIndex].title,
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xff6200ee),
+        unselectedItemColor: const Color(0xff757575),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: _navBarItems,
+      ),
+    );
+  }
+}
+
+final _navBarItems = [
+  SalomonBottomBarItem(
+    icon: const Icon(Icons.home),
+    title: const Text("Home"),
+    selectedColor: Colors.purple,
+  ),
+  SalomonBottomBarItem(
+    icon: const Icon(Icons.favorite_border),
+    title: const Text("Likes"),
+    selectedColor: Colors.pink,
+  ),
+  SalomonBottomBarItem(
+    icon: const Icon(Icons.search),
+    title: const Text("Search"),
+    selectedColor: Colors.orange,
+  ),
+  SalomonBottomBarItem(
+    icon: const Icon(Icons.person),
+    title: const Text("Profile"),
+    selectedColor: Colors.teal,
+  ),
+];
