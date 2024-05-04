9238951a9a15e378b1e083148cc3594d3876fd77
blackcat417 <ssoo174@ewha.ac.kr>
2024-05-04 10:52:07 +0900

    homehome

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screen/home.dart';
 import './screen/myRecipe.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AddRecipePage(),
+      home: HomePage(),
     );
   }
 }
